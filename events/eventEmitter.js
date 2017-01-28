200d108bed17dbb7de375cf0f5accdaab314c693
= <tbennett3434@gmail.com>
2017-01-28 13:22:45 -0700

    made edits to function names, declared constants

--- events/eventEmitter.js
+++ events/eventEmitter.js
@@ -1,23 +1,23 @@
-eventsInstance = require('events');
+const eventsInstance = require('events');
 var eventEmitter = new eventsInstance.EventEmitter();
 
 // Creating listener Function #1
-var ListenerFunction1 = function ListenerFunction1() {
+function listenerFunction11() {
   console.log('Execution completed for the Listener no. 1');
 };
 
 // Creating listener Function #2
-var ListenerFunction2 = function ListenerFunction2() {
+function listenerFunction12() {
  console.log('Execution completed for the Listener no. 2');
 };
 
-// Binding the integration event with the ListenerFunction1 function..
-eventEmitter.addListener('integration', ListenerFunction1);
+// Binding the integration event with the listenerFunction11 function..
+eventEmitter.addListener('integration', listenerFunction11);
 
-// Binding the integration event with the ListenerFunction2 function
-eventEmitter.on('integration', ListenerFunction2);
+// Binding the integration event with the listenerFunction12 function
+eventEmitter.on('integration', listenerFunction12);
 
-var eventListeners = require('events').EventEmitter.listenerCount
+var eventListeners = require('events').eventEmitter.listenerCount
   (eventEmitter,'integration');
 console.log(eventListeners + " Listener(s) are listening to the integration event");
 
@@ -25,13 +25,13 @@ console.log(eventListeners + " Listener(s) are listening to the integration even
 eventEmitter.emit('integration');
 
 // Remove the binding for the Listener function 1
-eventEmitter.removeListener('integration', ListenerFunction1);
-console.log("Listener function 1 will stop listen now.");
+eventEmitter.removeListener('integration', listenerFunction11);
+console.log("Listener function 1 will stop listening now.");
 
 // Fire the integration event
 eventEmitter.emit('integration');
 
-eventListeners = require('events').EventEmitter.listenerCount(eventEmitter,'integration');
+eventListeners = require('events').eventEmitter.listenerCount(eventEmitter,'integration');
 console.log(eventListeners + " Listener(s) are listening to integration event");
 
 console.log("Program execution completed successfully...");
